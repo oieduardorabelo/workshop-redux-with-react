6133a3232ea09fbcf98103e3a7f9901fbf11e90c
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-12-28 12:49:47 +1300

    reducer test

--- src/CommentList/CommentList.reducers.test.js
+++ src/CommentList/CommentList.reducers.test.js
@@ -3,20 +3,29 @@ import CommentListReducers from './CommentList.reducers'
 
 describe('CommentList.reducers', () => {
     it('returns default state when type doesnt match', () => {
-        const action = { type: 'FOO', payload: '' };
+        const action = { type: 'FOO', payload: '' }
         const reducer = CommentListReducers(undefined, action)
         expect(reducer).toEqual({ comments: [] })
     })
 
     it('when state is empty, SAVE_COMMENT add new comment', () => {
-        const action = { type: SAVE_COMMENT, payload: 'new comment' };
+        const payload = { id: Date.now(), text: 'new comment' }
+        const action = { type: SAVE_COMMENT, payload: payload }
         const reducer = CommentListReducers(undefined, action)
-        expect(reducer).toEqual({ comments: [ 'new comment' ] })
+        expect(reducer).toEqual({ comments: [ payload ] })
     })
 
     it('when state already have comment, SAVE_COMMENT concat new comment', () => {
-        const action = { type: SAVE_COMMENT, payload: 'other comment' };
-        const reducer = CommentListReducers({ comments: ['new comment'] }, action)
-        expect(reducer).toEqual({ comments: [ 'new comment', 'other comment' ] })
+        const payload = { id: Date.now(), text: 'new comment' }
+        const initialState = {
+          comments: [
+            { id: Date.now(), text: 'other comment' }
+          ]
+        }
+        const action = { type: SAVE_COMMENT, payload: payload }
+        const reducer = CommentListReducers(initialState, action)
+        const finalState = Object.assign({}, initialState)
+        finalState.comments = finalState.comments.concat(payload)
+        expect(reducer).toEqual(finalState)
     })
 })
